8ca505ab61b79eb622ab3ae109b6dd1c58496a5f
Jake Horner <jakehorner19@gmail.com>
2021-12-07 11:24:05 +0000

    nav bar & login custom

--- src/main/java/com/team/config/SecurityConfiguration.java
+++ src/main/java/com/team/config/SecurityConfiguration.java
@@ -2,32 +2,70 @@ package com.team.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
 
+import com.team.services.MyUserDetailsService;
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Autowired
-	UserDetailsService userDetailsService;
+	MyUserDetailsService userDetailsService;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-	}
+	@Bean
+    public AuthenticationProvider authenticationProvider() {
+		
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
 
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/admin").hasRole("ADMIN")
-			.antMatchers("/user").hasAnyRole("ADMIN","USER")
-			.antMatchers("/").permitAll()
-			.and().formLogin();
+		http
+//			.csrf().disable()
+//			.authorizeRequests().antMatchers("/login").permitAll()
+			.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/login").permitAll()
+			.failureUrl("/login-error.html")
+			.and()
+			.logout().invalidateHttpSession(true)
+			.clearAuthentication(true)
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/index").permitAll();
+//			.and()
+//			.authorizeRequests()
+//			.antMatchers("/admin").hasRole("ADMIN")
+//			.antMatchers("/user").hasAnyRole("ADMIN","USER")
+//			.antMatchers("/").permitAll()
+//			.antMatchers("/register").permitAll();
+
 	}
 	
 	@Bean
--- src/main/java/com/team/repos/UserRepo.java
+++ src/main/java/com/team/repos/UserRepo.java
@@ -1,13 +1,11 @@
 package com.team.repos;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.team.models.User;
 
 public interface UserRepo extends JpaRepository<User,Integer>{
 	
-	Optional<User> findByUserEmail(String userEmail);
+	User findByUserEmail(String userEmail);
 	
 }
